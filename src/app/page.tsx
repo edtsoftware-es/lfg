f62705f97dcff61aa58f9490f7b3dcd776892b1f
han <hander.juan@gmail.com>
2025-04-09 20:17:53 +0200

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+import { Button } from '@/components/ui/button';
+import { signOut } from '@/features/auth/actions';
 import { LoginForm } from '@/features/auth/ui/login-form';
 import { RegisterForm } from '@/features/auth/ui/register-form';
-import { getRoles } from '@/lib/queries';
+import { getRoles, getUser } from '@/lib/queries';
 
 export default async function Home() {
   const roles = await getRoles();
+  const user = await getUser();
 
   return (
     <main>
-      {JSON.stringify(roles, null, 2)}
-      <RegisterForm roles={[]} />
-
-      <h1>Sign in</h1>
-      <LoginForm />
+      {user ? (
+        <form>
+          <Button formAction={signOut}>Sign out</Button>
+        </form>
+      ) : (
+        <>
+          <RegisterForm roles={[]} />
+          <h1>Sign in</h1>
+          <LoginForm />
+        </>
+      )}
     </main>
   );
 }
